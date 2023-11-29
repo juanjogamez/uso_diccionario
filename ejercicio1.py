be79729fc93d29d601aacc5e0d2473f5b551dae4
alvaryto <alvaroav04@gmail.com>
2023-11-29 18:28:50 +0100

    implementacion eje 1

---
+++ ejercicio1.py
@@ -0,0 +1,30 @@
+import diccionario as di
+
+#try-step
+
+def replace_with_dictionary(texto):
+    dic=di.dictionary()
+    for element in dic:
+        texto=texto.replace(element, dic[element])
+    return texto
+
+def main():
+    try:
+        #marca de contexto#
+        with open('texto.txt', 'r') as file:
+            texto=file.read()
+
+    except FileNotFoundError:#es una variable global, el open la pone a uno si no encuentra el archivo
+        print("El archivo no se ha encontrado.")
+    except Exception as e:
+        print(e)
+    else:
+        print("lectura exsitosa")
+        texto=replace_with_dictionary(texto)
+        print(texto)
+
+main()
+
+
+
+
