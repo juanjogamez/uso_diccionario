c8f9897eab0d4e62726ab57c261f8fa40baaa9b9
alvaryto <alvaroav04@gmail.com>
2023-11-29 17:55:47 +0100

    Este es mi primer commit

---
+++ diccionario.py
@@ -0,0 +1,7 @@
+def dictionary():
+    return {
+        #clave : valor,
+        'ONU' : 'organizacion de las naciones unidas', 
+        'DDHH' : 'Derechos Humanos', 
+        'OMS' : 'Organizacion Mundial de la Salud'
+    }
