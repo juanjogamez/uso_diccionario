5117df1021bf32441c9143142150941b69b7d89d
alvaryto <152421073+alvaryto@users.noreply.github.com>
2023-11-29 18:00:07 +0100

    Update diccionario.py

--- diccionario.py
+++ diccionario.py
@@ -3,5 +3,6 @@ def dictionary():
         #clave : valor,
         'ONU' : 'organizacion de las naciones unidas', 
         'DDHH' : 'Derechos Humanos', 
-        'OMS' : 'Organizacion Mundial de la Salud'
+        'OMS' : 'Organizacion Mundial de la Salud', 
+        'FAO' : 'Organización de las Naciones Unidas para la Alimentación y la Agricultura'
     }
